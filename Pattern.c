875eb99bb14893774d316897a45abe4bfa3f0c4a
nayana <nayanasubash13@gmail.com>
2019-09-28 15:53:31 +0530

    First Commit

---
+++ Pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+(
+int i,j,n;
+printf("enter the value of n");
+scanf("%d",&n);
+
+for(int i=0;i<n;i++)
+{
+
+for(int j=row;j<=1;j--)
+{
+printf(" ");
+}
+for(k=1;k<=i;k++)
+printf("*");
+printf("\n");
+}
+
+
